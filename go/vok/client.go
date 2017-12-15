da3b6950b187ce6490e1c7a18de0662154c175a1
Janos Dobronszki <dobronszki@gmail.com>
2017-12-15 11:41:55 +0100

    Commit for build eJYGm91YY

---
+++ go/vok/client.go
@@ -0,0 +1,29 @@
+package vok
+
+import(
+	"github.com/1backend/go-client"
+)
+
+var Token string
+
+
+func GetHi() (string, error) {
+	var ret string
+	return ret, goclient.New(Token).Call("csicskavok", "vok", "GET", "/hi", map[string]interface{}{  }, &ret)
+}
+
+func GetImportedHi() (string, error) {
+	var ret string
+	return ret, goclient.New(Token).Call("csicskavok", "vok", "GET", "/imported-hi", map[string]interface{}{  }, &ret)
+}
+
+func PostInputExample(rect Rectangle, unit string) (Output, error) {
+	var ret Output
+	return ret, goclient.New(Token).Call("csicskavok", "vok", "POST", "/input-example", map[string]interface{}{ "rect": rect, "unit": unit }, &ret)
+}
+
+func GetSqlExample() error {
+	var ret 
+	return ret, goclient.New(Token).Call("csicskavok", "vok", "GET", "/sql-example", map[string]interface{}{  }, &ret)
+}
+
---
+++ ng/vok/src/vok.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { NgClient } from '@1backend/ng-client';
+
+
+
+
+@Injectable()
+export class Service {
+  constructor(private ngClient: NgClient) {}
+
+  getHi(): Promise<string> {
+    return this.ngClient.call<string>("csicskavok", "vok", "GET", "/hi", {  });
+  }
+
+  getImportedHi(): Promise<string> {
+    return this.ngClient.call<string>("csicskavok", "vok", "GET", "/imported-hi", {  });
+  }
+
+  postInputExample(rect: Rectangle, unit: string): Promise<Output> {
+    return this.ngClient.call<Output>("csicskavok", "vok", "POST", "/input-example", { "rect": rect, "unit": unit });
+  }
+
+  getSqlExample(): Promise<void> {
+    return this.ngClient.call<void>("csicskavok", "vok", "GET", "/sql-example", {  });
+  }
+
+}
